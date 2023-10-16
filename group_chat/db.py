928ff3ee71d7873eb7f6c691e05aaa8525ed2ec1
James Pringle <jameskpringle@gmail.com>
2023-10-16 13:52:56 -0500

    Update newuser DB code to check for username only

--- group_chat/db.py
+++ group_chat/db.py
@@ -7,6 +7,18 @@ FILE_NAME = "users.txt"
 ENCODING = "utf-8"
 
 
+def user_exists(user_id: str) -> bool:
+    """Check if the given user ID exists in the DB."""
+    path = Path(__file__).parent / Path(FILE_NAME)
+    if not path.exists():
+        return False
+    for line in path.read_text(encoding=ENCODING).splitlines():
+        split = line.split()
+        if len(split) == 2 and split[0] == user_id:
+            return True
+    return False
+
+
 def user_and_password_exists(user_id: str, password: str) -> bool:
     """Check if the given user ID and password exist in the DB."""
     path = Path(__file__).parent / Path(FILE_NAME)
@@ -22,7 +34,7 @@ def user_and_password_exists(user_id: str, password: str) -> bool:
 def insert_new_user_and_password(user_id: str, password: str) -> bool:
     """Add a new user ID and password to the DB."""
     path = Path(__file__).parent / Path(FILE_NAME)
-    if user_and_password_exists(user_id, password):
+    if user_exists(user_id):
         return False
     record = f"{user_id} {password}\n"
     with path.open("a", encoding=ENCODING) as f:
